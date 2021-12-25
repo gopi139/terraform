a321fc9cbead11ad4bf40495ac2052493844e6c6
saisuda9 <saikumarsuda@gmail.com>
2021-12-25 23:02:48 +0530

    adding new lines

--- output.tf
+++ output.tf
@@ -6,10 +6,34 @@ variable "abc1" {
   default = "100"
 }
 output "abc1" {
-  value = var.abc1
+  value = "value of abc1 = ${var.abc1}"
 }
 
 variable "abc2" {}
 output "abc2" {
   value = var.abc2
+}
+
+#string data type
+variable "sample1" {
+  default = "hello world"
+}
+output "sample1" {
+  value = var.sample1
+}
+
+#boolean data type
+variable "sample2" {
+  default = true
+}
+output "sample2" {
+  value = var.sample2
+}
+
+#number data type
+variable "sample3" {
+  default = 555
+}
+output "sample3" {
+  value = var.sample3
 }
