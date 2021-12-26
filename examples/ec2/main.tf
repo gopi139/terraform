2a1c6eb2a3bead7155f3c2ac9ac322745d56f7a0
saisuda9 <saikumarsuda@gmail.com>
2021-12-26 23:20:05 +0530

    adding new lines

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "sample" {
+  count         = length(var.name)
   ami           = "ami-0760b951ddb0c20c9"
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.SGID]
 
   tags = {
-    Name = var.name
+    Name = element(var.name, count.index )
   }
 }
 variable "SGID" {}
