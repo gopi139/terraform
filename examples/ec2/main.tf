6b7cef28f58df43f65f2bd133bda7b46d37e9339
saisuda9 <saikumarsuda@gmail.com>
2021-12-26 23:49:34 +0530

    adding new lines

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  count         = 0
+  count         = var.env == "PROD" ? 1 :0
   ami           = "ami-0760b951ddb0c20c9"
   instance_type =var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
@@ -12,3 +12,4 @@ variable "SGID" {}
 variable "name" {}
 
 variable "instance_type" {}
+variable "env" {}
