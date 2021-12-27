80abcd4c21194bd404ce4d283844b9c3f277f60e
saisuda9 <saikumarsuda@gmail.com>
2021-12-27 16:59:01 +0530

    adding new lines

---
+++ roboshop/main.tf
@@ -0,0 +1,18 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count         = length(var.components)
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0ca6b9c63c67b0dff"]
+
+  tags = {
+    Name = element(var.components, count.index )
+  }
+}
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "^Centos*"
+  owners      = ["973714476881"]
+}
+variable "components" {
+  default = ["frontend", "cart", "catalogue", "shipping", "mongodb", "payment", "mysql", "redis", "user", "rabbitmq"]
+}
