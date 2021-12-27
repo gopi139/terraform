ee6507a82662f049be525fd748398ffd5dac507b
saisuda9 <saikumarsuda@gmail.com>
2021-12-27 13:16:22 +0530

    adding new lines

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -7,6 +7,16 @@ resource "aws_instance" "sample" {
   tags = {
     Name = element(var.name, count.index )
   }
+  provisioner "remote-exec" {
+    connection {
+      host = "self.public_ip"
+      user = "root"
+      password = "DevOps321"
+    }
+    inline = [
+      "echo hello"
+    ]
+  }
 }
 variable "SGID" {}
 variable "name" {}
