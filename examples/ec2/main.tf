41b291a955d8148a3355aa78b315860c993d20fd
saisuda9 <saikumarsuda@gmail.com>
2021-12-27 12:55:11 +0530

    adding new lines

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   count         = var.env == "PROD" ? 1 :0
-  ami           = "ami-0760b951ddb0c20c9"
+  ami           = data.aws_ami.example.id
   instance_type =var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
@@ -13,3 +13,9 @@ variable "name" {}
 
 variable "instance_type" {}
 variable "env" {}
+
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "^ubuntu*"
+  owners      = ["973714476881"]
+}
