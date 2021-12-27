1853dd7f85841803f57bc2a195c1934f191f4185
saisuda9 <saikumarsuda@gmail.com>
2021-12-27 17:15:19 +0530

    adding new lines

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,13 +1,21 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  count         = length(var.components)
-  ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0ca6b9c63c67b0dff"]
-
+  wait_for_fulfillment   = true
   tags = {
     Name = element(var.components, count.index )
   }
 }
+resource "aws_ec2_tags" "tags" {
+  count       = length(var.components)
+  resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index )
+  key         = "Name"
+  value       = element(var.components, count.index )
+
+}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "^Centos*"
