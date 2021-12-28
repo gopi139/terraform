428164290a8994a2c8b897ca81168d1d31528a55
saisuda9 <saikumarsuda@gmail.com>
2021-12-28 17:45:18 +0530

    adding new lines

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -17,7 +17,7 @@ resource "aws_ec2_tag" "tags" {
 resource "aws_route53_record" "records" {
   count   = length(var.components)
   zone_id = Z02680521I7258TX51JJ3
-  name    = "${element(var.components,count.index)}.roboshop.internal"
+  name    = "${element(var.components,count.index)}-dev.roboshop.internal"
   type    = "A"
   ttl     = "300"
   records = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
